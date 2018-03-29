e948b2c032be4cfcad14dbbd08f44306fcb1ccd4
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-29 18:22:22 +0530

    In this step, you’ll add a stateful widget, RandomWords, which creates its State class, RandomWordsState. The State class will eventually maintain the proposed and favorite word pairs for the widget.
    Add the stateful RandomWords widget to main.dart. It can go anywhere in the file, outside of MyApp, but the solution places it at the bottom of the file. The RandomWords widget does little else besides creating its State class:

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+   createState() => new RandomWordsState();
+      
+   }
+   
+   class RandomWordsState extends State<RandomWords> {
+     @override
+       Widget build(BuildContext context) {
+         final wordPair = new WordPair.random();
+         return new Text(wordPair.asPascalCase);
+       }
+}
