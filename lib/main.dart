11bca0eaf17a1a598e0e5f549073a168a3566101
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-29 10:48:02 +0530

    using the  flutter get packges and below the icons wrote the package name English_words:^3.1.0 dependency and import into the dart file .

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final WordWrap = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter Akhielsh',
       home: new Scaffold(
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter from Appbar title'),
         ),
         body: new Center(
-          child: new Text("Akhilesh r u in center."),
+          child: new Text(WordWrap.asPascalCase),
         ),
       ),
     );
